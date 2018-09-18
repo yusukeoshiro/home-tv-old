bba56484fa55abac75672f0d2f397b9f3c0c0ef0
Yusuke Oshiro <yusuke@oshiro1.com>
2018-09-19 05:15:54 +0900

    modified authcontroller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -13,8 +13,12 @@ class AuthController < ApplicationController
     def callback
 
         begin
+            uri = URI.parse( request.original_url )
+            my_host = ( (uri.port == 443) ? "https://" : "http://" ) + uri.host + (( (uri.port.to_i == 80) || (uri.port.to_i == 443) ) ? "" : ":#{uri.port}")
+
+
             code = params[:code]
-            $access_token = $google_auth_client.auth_code.get_token(code, :redirect_uri => 'http://localhost:3000/auth/callback')
+            $access_token = $google_auth_client.auth_code.get_token(code, :redirect_uri => "#{my_host}/auth/callback")
             $redis.set("google_auth_hash", $access_token.to_hash.to_json)
             puts $access_token.token
             redirect_to "/"
