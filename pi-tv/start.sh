86d0612add6c092497163b6e6e762d02e2e81dfa
Yusuke Oshiro <yusuke@oshiro1.com>
2018-12-16 12:57:33 +0900

    modified start script

--- pi-tv/start.sh
+++ pi-tv/start.sh
@@ -7,7 +7,8 @@ kill -9 $(pidof ruby watch.rb)
 
 cd /home/pi/home-tv/pi-tv
 cd lib && ruby main.rb | tee logs/log.main &
-cd lib && ruby sftp.rb | tee logs/log.sftp &
-recpt1 --device /dev/px4video3 --b25 --strip --sid hd --http 8888 | tee logs/log.tv &
+cd lib && ruby convert.rb | tee logs/log.convert &
+cd lib && ruby upload.rb | tee logs/log.upload &
+recpt1 --device /dev/px4video2 --b25 --strip --sid hd --http 8888 | tee logs/log.tv &
 
 exit 0
