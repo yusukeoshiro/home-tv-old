a84f4dcc2285cb2c15afa84cb5a533906c0725c0
Yusuke Oshiro <yusuke@oshiro1.com>
2018-12-16 14:57:50 +0900

    added show update on util.rb

--- pi-tv/lib/util.rb
+++ pi-tv/lib/util.rb
@@ -1,28 +1,38 @@
 require 'net/http'
-require "dotenv"
-require "json"
+require 'dotenv'
+require 'json'
 
 Dotenv.load
 
-def notify message
+def notify(message)
+  uri = URI(ENV['NOTIFY_URL'])
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  req = Net::HTTP::Post.new(uri.path)
+  req.body = {
+    'value1' => message
+  }.to_json
+  req['content-type'] = 'application/json'
+  result = https.request(req)
+  raise result.body if result.code.to_i != 200
 
-	begin
-		uri = URI(ENV["NOTIFY_URL"])
-		https = Net::HTTP.new(uri.host, uri.port)
-		https.use_ssl = true
-		req = Net::HTTP::Post.new(uri.path)
-		req.body = {
-			"value1" => message
-		}.to_json
-		req["content-type"] = "application/json"
-		result = https.request(req)
-		if result.code.to_i != 200
-			raise result.body
-		end
-	rescue => e
-		puts "error occured while sending push notification to IFTTT!!"
-		puts e.message
-	end
+  message
+rescue => e
+  puts 'error occured while sending push notification to IFTTT!!'
+  puts e.message
+end
 
-	return message
+def update_recording_job(show_uuid, status)
+  uri = URI(ENV['UPDATE_URL'].gsub(':UUID', show_uuid))
+  https = Net::HTTP.new(uri.host, uri.port)
+  req = Net::HTTP::Post.new(uri.path)
+  req.body = {
+    'finished' => status
+  }.to_json
+  req['content-type'] = 'application/json'
+  result = https.request(req)
+  raise result.body if result.code.to_i != 200
+rescue => e
+  puts 'something went wrong...'
+  puts e.message
 end
