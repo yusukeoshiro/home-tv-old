e0c7f196b35856a42c1940128bb4c9c5b5c2f8b2
Yusuke Oshiro <yusuke@oshiro1.com>
2019-06-16 14:12:29 +0900

    removed show fetcher

--- app/workers/fetch_scheduler.rb
+++ app/workers/fetch_scheduler.rb
@@ -4,9 +4,15 @@ class FetchScheduler
   include Sidekiq::Worker
 
   def perform
+    util = ShowFetchUtil.new
     7.times do |i|
-      ShowFetcher.perform_async Date.today + i
+      begin
+        fetch_date = Date.today + i
+        puts "fetching #{fetch_date}..."
+        util.fetch(fetch_date)
+      rescue => e
+        puts e.message
+      end
     end
   end
-
 end
